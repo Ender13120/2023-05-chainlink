da00b97fe805e04314c178ae609dfb9bdab0ed58
Ender13120 <117198798+Ender13120@users.noreply.github.com>
2024-10-18 15:20:39 +0200

    chore: update ERC677 to Openzepplin V5

--- contracts/pools/tokens/ERC677.sol
+++ contracts/pools/tokens/ERC677.sol
@@ -12,15 +12,20 @@ contract ERC677 is IERC677, ERC20 {
 
   constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
-  /// @inheritdoc IERC677
-  function transferAndCall(address to, uint amount, bytes memory data) public returns (bool success) {
-    super.transfer(to, amount);
-    emit Transfer(msg.sender, to, amount, data);
-    if (to.isContract()) {
-      IERC677Receiver(to).onTokenTransfer(msg.sender, amount, data);
+    /// @inheritdoc IERC677
+    function transferAndCall(address to, uint256 amount, bytes memory data)
+        public
+        returns (bool success)
+    {
+        super.transfer(to, amount);
+        emit Transfer(msg.sender, to, amount, data);
+        //@notice .isContract from OZ4 was deprecated, which is why we replaced it with code.length;
+        if (address(to).code.length != 0) {
+            IERC677Receiver(to).onTokenTransfer(msg.sender, amount, data);
+        }
+        return true;
     }
-    return true;
-  }
+
 
   /// @inheritdoc IERC677
   function transferAndCall(address to, uint256 amount) external returns (bool) {
